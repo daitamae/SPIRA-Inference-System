863368a5073f98cbb6bde0d000b6f9870d935e74
daitamae <dai.tamae@gmail.com>
2022-12-05 20:19:03 -0300

    Correct api service for deploy

--- build-api.sh
+++ build-api.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-chmod +x ./wait-for-it.sh
-docker-compose -f docker-compose.api.yml up --build api mongo mongo-express nats minio mlflow-mc mlflow db
+docker compose -f docker-compose.api.yml up --build api mongo mongo-express nats minio mlflow-mc mlflow db
